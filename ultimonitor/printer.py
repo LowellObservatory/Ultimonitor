3463289124d58a965825e1961d5a58677a84fa11
Ryan T. Hamilton <astrobokonon@gmail.com>
2020-01-13 16:14:06 -0700

    Wrong place to hack! Fixed

--- ultimonitor/printer.py
+++ ultimonitor/printer.py
@@ -155,12 +155,13 @@ def formatStatus(stats):
                              'Extruder1Temp', 'Extruder1Setp',
                              'Extruder2Temp', 'Extruder2Setp']:
                     retStr += "\t%s: %.3f C\n" % (key, stats[sect][key])
-                elif key == 'CalculationTime':
-                    retStr += "%s: %.3f C\n\n" % (key, stats[sect][key])
                 else:
                     retStr += "\t%s: %s\n" % (key, stats[sect][key])
         elif isinstance(stats[sect], str):
-            retStr += "\t%s: %s\n" % (sect, stats[sect])
+            if sect == "CalculationTime":
+                retStr += "%s: %.3f C\n\n" % (sect, stats[sect])
+            else:
+                retStr += "\t%s: %s\n" % (sect, stats[sect])
         elif isinstance(stats[sect], float):
             retStr += "\t%s: %f\n" % (sect, stats[sect])
 
